97766df0b1bc276321cb541f2b275affcadd763a
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-27 13:33:47 +0200

    Add function display discount

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,16 @@ Phone.prototype.printInfo = function() {
   console.log("The phone brand is " + this.brand + ", color is " + this.color +
     " and the price is " + this.price + ".");
 }
+Phone.prototype.applyDiscount = function(discount) {
+    this.price = this.price - discount;
+    
+  
+} 
 var iPhone6S = new Phone("Apple", 2250, "silver");
 var samsungGalaxyS6 = new Phone("Samsung", 2000, "black");
 var onePlusOne = new Phone("OnePlusOne", 1800, "gold")
 iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+onePlusOne.applyDiscount(100);
 onePlusOne.printInfo();
