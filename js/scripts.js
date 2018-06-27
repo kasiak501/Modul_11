353b6e4fafe5b6c42170b2ac26012077e53d8a65
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-27 23:04:29 +0200

    Fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-  this.brand = brand; //by using this, the "brand" property of the object we create will assume the value of the brand argument
-  this.price = price;
-  this.color = color;
+function Phone(brand, price, color) {
+	this.brand = brand; //by using this, the "brand" property of the object we create will assume the value of the brand argument
+	this.price = price;
+	this.color = color;
 }
 Phone.prototype.printInfo = function() {
-  console.log("The phone brand is " + this.brand + ", color is " + this.color +
-    " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color +
+		" and the price is " + this.price + ".");
 }
 Phone.prototype.applyDiscount = function(discount) {
-  this.price = this.price - discount;
+	this.price = this.price - discount;
 }
 var iPhone6S = new Phone("Apple", 2250, "silver");
 var samsungGalaxyS6 = new Phone("Samsung", 2000, "black");
